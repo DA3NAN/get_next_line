efc5e5f14c93a7ad60143072f2bfb8be2495a902
Adnane Ait Malek <aait-mal@e2r11p2.1337.ma>
2022-11-10 20:55:48 +0100

    problem solved :)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 18:34:30 by aait-mal          #+#    #+#             */
-/*   Updated: 2022/11/10 15:10:19 by aait-mal         ###   ########.fr       */
+/*   Updated: 2022/11/10 20:55:07 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,10 @@ static char	*get_whole_string(int fd, char *string)
 		if (bytes_read == -1)
 		{
 			free(buff);
+			free(string);
 			return (NULL);
 		}
 		buff[bytes_read] = '\0';
-		if (!string)
-		{
-			string = malloc(1 * sizeof(char));
-			string[0] = '\0';
-		}
 		string = ft_strjoin(string, buff);
 	}
 	free(buff);
@@ -66,9 +62,17 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
+	if (!string)
+	{
+		string = malloc(1 * sizeof(char));
+		string[0] = '\0';
+	}
 	string = get_whole_string(fd, string);
 	if (!string)
+	{
+		free(string);
 		return (0);
+	}
 	line = get_new_line(string);
 	string = get_left_string(string);
 	return (line);
