bc7910e5e0c4cc40bbc800dfffb91ddefbdacf72
Adnane Ait Malek <aait-mal@e2r11p2.1337.ma>
2022-11-11 16:12:17 +0100

    Mandatory done :)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 18:34:30 by aait-mal          #+#    #+#             */
-/*   Updated: 2022/11/11 10:32:28 by aait-mal         ###   ########.fr       */
+/*   Updated: 2022/11/11 16:07:41 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,10 +69,7 @@ char	*get_next_line(int fd)
 	}
 	string = get_whole_string(fd, string);
 	if (!string)
-	{
-		free(string);
-		return (0);
-	}
+		return (NULL);
 	line = get_new_line(string);
 	string = get_left_string(string);
 	return (line);
