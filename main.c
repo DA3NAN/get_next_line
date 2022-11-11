bc03f888e77bed004ee766cb67ba38be7871c599
Adnane Ait Malek <aait-mal@e2r11p2.1337.ma>
2022-11-11 10:33:54 +0100

    Mandatory done :)

--- main.c
+++ main.c
@@ -6,26 +6,26 @@
 /*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 18:34:22 by aait-mal          #+#    #+#             */
-/*   Updated: 2022/11/10 20:40:22 by aait-mal         ###   ########.fr       */
+/*   Updated: 2022/11/11 10:32:44 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"get_next_line.h"
 
-// int	main(void)
-// {
-// 	int		fd;
+int	main(void)
+{
+	int		fd;
 
-// 	fd = open("covid.txt", O_RDWR | O_CREAT);
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	printf("{{ %s }}", get_next_line(fd));
-// 	return (0);
-// }
+	fd = open("covid.txt", O_RDWR | O_CREAT);
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	printf("{{ %s }}", get_next_line(fd));
+	return (0);
+}
