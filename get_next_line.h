5cb7d502b67bdd34eee1393fb1a1c57804f6c5e1
Adnane Ait Malek <aait-mal@e2r9p2.1337.ma>
2022-11-16 11:11:06 +0100

    Bonus added :)

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 18:34:25 by aait-mal          #+#    #+#             */
-/*   Updated: 2022/11/15 11:50:49 by aait-mal         ###   ########.fr       */
+/*   Updated: 2022/11/16 11:03:02 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
 # include<unistd.h>
 # include<stdlib.h>
 # include<fcntl.h>
